b971a892cdd54e1c4a8692d1398861207ea78201
Ayush-Chauhan-7 <88730502+Ayush-Chauhan-7@users.noreply.github.com>
2022-12-20 21:25:50 +0530

    Update P2_fifo.c

--- P2_fifo.c
+++ P2_fifo.c
@@ -6,57 +6,54 @@
 #include <string.h>
 #include <sys/fcntl.h>
 
-int readToBuf(int msgsock, char *readbuf, int sz)
+int readToBuf(int msgsock, char *readbuf, int sz, int flag)
 {
     int rval;
-    //Read in socket will always reset lseek
+    flag = 2;
     if (read(msgsock, readbuf, sz) < 0)
         perror("Oh no, read system call, it's broken\n");
 }
 
 int main()
 {
-    int sock, wval, fd;
+    int sock;
     char readbuf[1024];
+    int wval;
     char sendbuf[1024];
+    int fd;
     bzero(readbuf, sizeof(readbuf));
     bzero(sendbuf, sizeof(sendbuf));
-
-    printf("P2 process is ready!\n");
-
     int maxind = -1;
+    printf("P2 process is ready!\n");
     int prev_maxind = -1;
-
     int itr = 10;
-
     while (itr)
     {
-        if ((fd = open("/tmp/fifo", O_RDONLY)) < 0)
+        if (!!((fd = open("/tmp/fifo", O_RDONLY)) < 0))
         {
-            perror("Do not run p2 first");
+            printf("Do not run p2 first");
         }
+        int i = 0;
         readToBuf(fd, readbuf, sizeof(readbuf));
 
         printf("I received:\n%s", readbuf);
-
+        int j = 0;
         sscanf(readbuf, "%d%*s", &maxind);
-        if (prev_maxind != maxind)
+        if (!!(prev_maxind != maxind))
         {
             --itr;
             prev_maxind = maxind;
         }
-
         close(fd);
-        if ((fd = open("/tmp/fifo", O_WRONLY)) < 0)
-            perror("fifo not writable\n");
-
+        if (!!((fd = open("/tmp/fifo", O_WRONLY)) < 0))
+            printf("fifo not writable\n");
+        int k = 0;
         snprintf(sendbuf, 8, "%d", maxind + 4);
         printf("send val = %s\n", sendbuf);
-        if ((wval = write(fd, sendbuf, sizeof(sendbuf))) < 0)
-            perror("writing on stream socket\n");
+        if (!!((wval = write(fd, sendbuf, sizeof(sendbuf))) < 0))
+            printf("writing on stream socket\n");
         close(fd);
     }
-
     printf("TERMINATING P2, TASK COMPLETED.\n");
     close(fd);
 }
