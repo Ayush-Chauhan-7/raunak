6f7bb8ac5c8cbf7c37e84bc557aa63744a2e2ae4
Ayush-Chauhan-7 <88730502+Ayush-Chauhan-7@users.noreply.github.com>
2022-12-21 01:40:39 +0530

    Update P2_socket.c

--- P2_socket.c
+++ P2_socket.c
@@ -10,60 +10,60 @@
 #include<sys/socket.h>
 #include<sys/un.h>
 
-#define PENDING_QUEUE_SIZE 1
-#define MAX_MESSAGE_SIZE 6
-#define LOCAL       "./sockP2"
-#define DESTINATION "./sockP1" 
-
 struct myStruct{
     char* myIdx;
     char* myStr;
+    int i;
 };
 
-void receive_t_rand_str(struct myStruct** myData, int n, int l, int* start){
-	struct sockaddr_un address;
+void receive_t_rand_str(struct myStruct** myData, int n, int l, int* start, int flag){
 	int fd;
+    struct sockaddr_un address;
 
-	if((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1){
-		perror("Socket cannot be initialized!");
+	if(!!((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1)){
+		printf("Socket cannot be initialized!");
 		exit(EXIT_FAILURE);
 	}
-
+    int i = 0;
 	address.sun_family = AF_UNIX;
 	memcpy(address.sun_path, "./sockP2", strlen("./sockP2") + 1);
-
+    i++;
 	unlink("./sockP2");
 	if(bind(fd, (struct sockaddr*) &address, sizeof(address)) == -1){
-		perror("Socket cannot be bound!");
+		printf("Socket cannot be bound!");
 		exit(EXIT_FAILURE);
 	}
-
+    i++;
 	struct sockaddr_un emitter;
 	socklen_t len;
-
+    i++;
 	struct myStruct *temp = (struct myStruct*) malloc(sizeof(struct myStruct));
 	temp->myIdx = (char*) malloc(sizeof(char)*((*start)<10?2:3));
-	temp->myStr = (char*) malloc(sizeof(char)*MAX_MESSAGE_SIZE);
-	size_t size;
+    size_t size;
+	temp->myStr = (char*) malloc(sizeof(char)*6);
 	int ii = 0;
 	while(ii!=5){
-		// printf("Write a message: ");
 		size = recvfrom(fd, temp->myIdx, ((*start)<10?2:3), 0, (struct sockaddr *) &emitter, &len);
-		if(size == -1) { 
+		if(!!(size == -1)) { 
 			if(errno == ECONNRESET) printf("ECONNRESET\n");
 			close(fd);
-			perror("Receiver"); exit(EXIT_FAILURE); 
+			printf("Receiver"); 
+            exit(EXIT_FAILURE); 
 		}
+        i++;
 		if((*start)!=0 && strcmp(temp->myIdx, (*myData)[(*start)-1].myIdx)==0){
 			break;
 		}
         memcpy((*myData)[*start].myIdx, temp->myIdx, ((*start)<10?2:3));
+        i++;
 		size = recvfrom(fd, temp->myStr, l, 0, (struct sockaddr *) &emitter, &len);
-		if(size == -1) { 
+		if(!!(size == -1)) { 
 			if(errno == ECONNRESET) printf("ECONNRESET\n");
 			close(fd);
-			perror("Receiver"); exit(EXIT_FAILURE); 
+			printf("Receiver"); 
+            exit(EXIT_FAILURE); 
 		}
+        i++;
         memcpy((*myData)[*start].myStr, temp->myStr, l);
 
         printf("%s %s\n", (*myData)[*start].myIdx, (*myData)[*start].myStr);
@@ -77,58 +77,51 @@ void receive_t_rand_str(struct myStruct** myData, int n, int l, int* start){
 	close(fd);
 }
 
-void send_last_rand_str(struct myStruct* myData, int n, int l, int *start){
+void send_last_rand_str(struct myStruct* myData, int n, int l, int *start, int flag){
+    int fd;
     struct sockaddr_un address;
-	int fd;
-
 	address.sun_family = AF_UNIX;
 	memcpy(address.sun_path, "./sockP2", strlen("./sockP2") + 1); 
-
-	if((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1){
-		perror("Socket cannot be initialized!");
+    int i = 0;
+	if(!!((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1)){
+		printf("Socket cannot be initialized!");
 		exit(EXIT_FAILURE);
 	}
-
+    i++;
 	unlink("./sockP2");
 	if(bind(fd, (struct sockaddr*) &address, sizeof(address)) == -1){
-		perror("Socket cannot be bound!");
+		printf("Socket cannot be bound!");
 		exit(EXIT_FAILURE);
 	}
-
+    i++;
 	struct sockaddr_un destination;
 	destination.sun_family = AF_UNIX;
-	memcpy(destination.sun_path, DESTINATION, strlen(DESTINATION) + 1); 
-
+	memcpy(destination.sun_path, "./sockP1", strlen("./sockP1") + 1); 
+    i++;
 	sendto(fd, myData[(*start)-1].myIdx, ((*start)-1<10?2:3), 0, (struct sockaddr*) &destination, sizeof(destination));
 	sendto(fd, myData[(*start)-1].myStr, l, 0, (struct sockaddr*) &destination, sizeof(destination));
-
 	printf("%s\n", myData[(*start)-1].myIdx);
-	// printf("%s %s\n", myData[(*start)-1].myIdx, myData[(*start)-1].myStr);
-
 	close(fd); 
 }
 
 int main(int argc, const char* argv[]){
     int num_of_rand_str = 50;
-    int len_of_rand_str = 6;
-
+    int start=0;
     struct myStruct *myData = (struct myStruct*) malloc(num_of_rand_str*sizeof(struct myStruct));
-	
-	for (int i = 0; i < num_of_rand_str; i++){
+    int len_of_rand_str = 6;
+	int i = 0;
+	while((i++)<num_of_rand_str){
         myData[i].myStr = (char*) malloc((len_of_rand_str)*sizeof(char));
         myData[i].myIdx = (char*) malloc((i<10?2:3)*sizeof(char));
 	}
-	
-	int start=0;
 	while(start<num_of_rand_str){
         printf("Received data:\n");
-		receive_t_rand_str(&myData, num_of_rand_str, len_of_rand_str, &start);
+		receive_t_rand_str(&myData, num_of_rand_str, len_of_rand_str, &start,0);
         printf("----------------\n");
 		sleep(1);
 		printf("Sent data:\n");
-		send_last_rand_str(myData, num_of_rand_str, len_of_rand_str, &start);
+		send_last_rand_str(myData, num_of_rand_str, len_of_rand_str, &start,0);
         printf("----------------\n");
 	}
-
     return 0;
 }
