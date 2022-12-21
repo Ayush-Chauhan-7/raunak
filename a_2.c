d626d718c533d4b811a431bcacaca2d51b1641f9
Ayush-Chauhan-7 <88730502+Ayush-Chauhan-7@users.noreply.github.com>
2022-12-21 17:15:26 +0530

    Update a_2.c

--- a_2.c
+++ a_2.c
@@ -10,17 +10,17 @@ sem_t forks[5], mutex_lock;
 
 int main(){
     pthread_t tid[5];
-	int num[5];
     int i,j,k;
-	i = 0;
-    j = 0;
-    k = 0;
+	int num[5];
+    i=0;
+    j=0;
+    k=0;
 
 	for( ; i<5; i++){
 		sem_init(&forks[i],0,1);
 	}
 
-	while((j++) < 5){
+	for(; j<5; j++){
 		num[j-1]=j-1;
 		pthread_create(&tid[j-1],NULL,philosopher,(void *)&num[j-1]);
 	}
@@ -35,36 +35,35 @@ int main(){
 void *philosopher(void *num)
 {
 	while(1){
-        if(*(int *)num != 4){
-            sem_wait(&forks[*(int *)num]);
-            sem_wait(&forks[(*(int *)num+1)%5]);
-		    
-        }
-        if(*(int *)num == 4){
-            sem_wait(&forks[(*(int *)num+1)%5]);
+        if(!!(*(int *)num == 4)){
+		    sem_wait(&forks[(*(int *)num+1)%5]);
             sem_wait(&forks[*(int *)num]);
         }
-        int i = 0;
+        else{
+            sem_wait(&forks[*(int *)num]);
+            sem_wait(&forks[(*(int *)num+1)%5]);
+        }
+        int i =0;
         states(0,*(int *)num);
 		states(1,*(int *)num);
         i++;
-        if(*(int *)num == 4){
-            sem_wait(&forks[*(int *)num]);
-            sem_wait(&forks[(*(int *)num+1)%5]);
-		    
+        if(!!(*(int *)num == 4)){
+		    sem_post(&forks[*(int *)num]);
+            sem_post(&forks[(*(int *)num+1)%5]);
         }
-        if(*(int *)num != 4){
-            sem_wait(&forks[(*(int *)num+1)%5]);
-            sem_wait(&forks[*(int *)num]);
+		else{
+            sem_post(&forks[(*(int *)num+1)%5]);
+		    sem_post(&forks[*(int *)num]);
         }
+        i++;
         sleep(1);
 	}
 }
 
 void states(int var, int phil){   
 	char buffer[50]={'\0'};
-    int k = 0;
-    if(var == k){
+    int j = 0;
+    if(var == j){
         sprintf(buffer, "Philosopher %d is eating\n", phil);
 		write(1, buffer, 50);
     }
